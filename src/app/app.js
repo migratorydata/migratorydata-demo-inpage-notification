230e848ee4164b1b722fed66c067aba38beba41b
Mihai Rotaru <mihai.rotaru@migratorydata.com>
2023-08-14 15:42:11 +0200

    save modifications for website demo

--- src/app/app.js
+++ src/app/app.js
@@ -2,13 +2,36 @@ const MigratoryDataClient = require("migratorydata-client");
 const ClipboardJS = require('clipboard');
 
 // the default address of the MigratoryData Server installed on local machine.
-var SERVER = "https://demo.migratorydata.com";
+var SERVER = "https://cloud.migratorydata.com";
 
 // the subject used to receive messages fromMigratoryData Server.
-var SUBJECT = "/push/notification";
+var SUBJECT = "/migratorydata/notification/" + getPushNotificationDemoId();
 
 // default EntitlementToken used in MigratoryData Server.
-var TOKEN = "some-token";
+var TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI1NzhmZDAiLCJwZXJtaXNzaW9ucyI6eyJhbGwiOlsiL21pZ3JhdG9yeWRhdGEvbm90aWZpY2F0aW9uLyoiXX0sImFwcCI6Im5vdGlmaWNhdGlvbiIsImlhdCI6MTY5MTYwMTE2OCwiZXhwIjoxNzIyNzA1MTY4fQ.C9yGVC6j0w-mscbYyXEWQ8Nf-8GFuqjNj0SoOOiU4Ow";
+
+function generateRandomId(length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+
+function getPushNotificationDemoId() {
+  var id = localStorage.getItem('notificationDemoId');
+  if (id) {
+    return id;
+  }
+
+  id = "id_" + generateRandomId(4);
+  localStorage.setItem('notificationDemoId', id);
+  return id;
+};
+
 
 document.addEventListener('DOMContentLoaded', function() {
 	initUI();
